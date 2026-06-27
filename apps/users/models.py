320d34bf1f9c6d9ebfa0f2003c1b6fd17c9a9d91
Jeslor Ssozi <jbestius@gmail.com>
2026-06-27 19:49:17 +0200

    update the database url

--- apps/users/models.py
+++ apps/users/models.py
@@ -7,14 +7,14 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=255)
     is_manager = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
 
     def __str__(self):
         return self.email
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField('users.CustomUser', on_delete=models.CASCADE, related_name='profile')
 
     profile_bio = models.TextField()
     profile_photo_url = models.URLField(blank=True)
